4457278714312528142503bee7799ef477fc34dd
Milos Lajtman <milos.lajtman@edocu.com>
2019-10-19 14:55:43 +0200

    Wrap clicked/targeted reflist item in red highlight frame

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,4 +1,6 @@
-import { darken, saturate } from 'unitransform';
+import { darken, lighten, saturate, desaturate } from 'unitransform';
 
 export const red = `#c10000`;
 export const redDark = saturate(darken(red, 12), 5);
+export const redLight = desaturate(lighten(red, 24), 10);
+export const redExtraLight = desaturate(lighten(red, 52), 25);
