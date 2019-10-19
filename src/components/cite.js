8790a1079ed0ccc46bd3797ccee8c6a564821b1e
Milos Lajtman <milos.lajtman@edocu.com>
2019-10-19 15:55:34 +0200

    Display tooltip with full citation using react-tooltip

--- src/components/cite.js
+++ src/components/cite.js
@@ -1,18 +1,56 @@
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 import React, { memo } from 'react';
+import ReactTooltip from 'react-tooltip';
 import { citations } from './reflist';
 
-const Cite = ({ name }) => (
-  <sup
-    css={css`
-      font-weight: regular;
-      padding: 0 1px;
-    `}
-  >
-    <a href={`#ref-${name}`}>{citations.findIndex(x => x.name === name) + 1}</a>
-  </sup>
-);
+const Cite = ({ name }) => {
+  const citation = citations.find(x => x.name === name);
+  const tooltipId = Math.round(Math.random() * 999999999);
+
+  return (
+    <sup
+      css={css`
+        font-weight: regular;
+        padding: 0 1px;
+      `}
+    >
+      <a
+        data-tip
+        data-for={`tooltip-${name}-${tooltipId}`}
+        href={`#ref-${name}`}
+      >
+        {citations.findIndex(x => x.name === name) + 1}
+      </a>
+      <ReactTooltip
+        id={`tooltip-${name}-${tooltipId}`}
+        aria-haspopup="true"
+        type="error"
+        effect="solid"
+        delayHide={1000}
+        css={css`
+          max-width: 350px;
+          line-height: 1.5;
+
+          p {
+            font-family: 'Open Sans', sans-serif;
+            font-size: 12px;
+          }
+        `}
+      >
+        <p>{citation.citation}</p>
+
+        {citation.url && (
+          <p>
+            <a href={citation.url} rel="external noopener">
+              More information
+            </a>
+          </p>
+        )}
+      </ReactTooltip>
+    </sup>
+  );
+};
 
 Cite.propTypes = { name: PropTypes.string.isRequired };
 
